f7cbf41aa3d688db690981bff3ccec9ef898b481
ShotRep <78008209+ShotRep@users.noreply.github.com>
2021-06-11 07:49:07 -0500

    incremental commit

--- index.js
+++ index.js
@@ -226,9 +226,12 @@ let myArray = [(artists[8].name = "Vincent van Gogh")];
  
  Example, if getArtistByIndex is invoked with the artists array and the number 0, it will return `the artist at index 0 is Amedeo Modigliani` */
 
-function getArtistByIndex(/*Your Code Here*/) {
-  /*Your Code Here*/
+function getArtistByIndex(array, index) {
+  for (let i = 0; i < array.length; i++) {
+    return `the artist at index ${array[index].id} is ${array[index].name}`;
+  }
 }
+getArtistByIndex(artists, 0);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use get20s to do the following: 
@@ -238,9 +241,16 @@ Use get20s to do the following:
 Example born in 1901 and died in 1959 - included -- born in 1889 and died in 1925 not included
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/*Your Code Here*/) {
-  /*Your Code Here*/
+function get20s(array) {
+  let twentiethCentury = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].years >= "1900" && array[i].years <= "2000") {
+      twentiethCentury.push(array[i]);
+    }
+  }
+  return twentiethCentury;
 }
+get20s(artists);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
  Use removeArtist to do the following:
